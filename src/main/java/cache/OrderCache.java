2b23429577c74dbb3eab8390c6ba12ddd9e4e3df
alihan <alihan1996@live.com>
2018-12-19 19:37:16 +0100

    User cache fixed

--- src/main/java/cache/OrderCache.java
+++ src/main/java/cache/OrderCache.java
@@ -14,12 +14,12 @@ public class OrderCache {
     private ArrayList<Order> orders;
 
     //Tiden som vores cache burde kunne køre
-    private long timelimit;
+    private long ttl;
 
     //Herefter sætter vi tiden hvor vores cache blev oprettet
-    private long create;
+    private long created;
 
-    public OrderCache() { this.timelimit = Config.getOrderTimelimit(); }
+    public OrderCache() { this.ttl = Config.getOrderTimelimit(); }
     public ArrayList<Order> getProducts(Boolean forceUpdate)
     {
         //Hvis vi her clearer vores cache, kan vi altså forcere opdateringen
@@ -27,7 +27,7 @@ public class OrderCache {
         //Hvis det er således at listen er tom kan vi også tjekke efter nye produkter
 
         if (forceUpdate
-              || ((this.create + this.timelimit) >= (System.currentTimeMillis() / 1000L))
+              || ((this.created + this.ttl) >= (System.currentTimeMillis() / 1000L))
               || this.orders.isEmpty()) {
 
             //Da vi gerne vil opdatere så getter vi vores produkter fra vores controller
@@ -35,7 +35,7 @@ public class OrderCache {
 
             //Nu setter vi produkterne for instancen og setter samtidig også vores oprettede timestamp
             this.orders = orders;
-            this.create = System.currentTimeMillis() / 1000L;
+            this.created = System.currentTimeMillis() / 1000L;
         }
 
         //Nu returnerer vi vores dokumenter
--- src/main/java/cache/UserCache.java
+++ src/main/java/cache/UserCache.java
@@ -1,6 +1,50 @@
 package cache;
 
-//TODO: Build this cache and use it.
+import controllers.UserController;
+import model.User;
+import utils.Config;
+
+import java.util.ArrayList;
+
+//TODO: Build this cache and use it. - FIXED
 public class UserCache {
 
-}
+    //Produktliste
+    private ArrayList<User> users;
+
+    //Tiden som vores cache bør kunne overleve i
+    private long ttl;
+
+    //Bliver sat når vores cache oprettes
+    private long created;
+
+    public UserCache() {
+        this.ttl = Config.getUserTtl(); }
+
+    public ArrayList<User> getUsers(Boolean forceUpdate)
+    {
+
+        //Hvis vi her clearer vores cache, kan vi altså forcere opdateringen
+        //Evt. kan vi se på "alderen" af vores cache og dermed finde ud af om vi skal opdatere
+        //Hvis det er således at listen er tom kan vi også tjekke efter nye produkter
+
+        if (forceUpdate
+                || ((this.created + this.ttl) >= (System.currentTimeMillis() / 1000L ))
+                || this.users.isEmpty()) {
+            //Vi getter vores produkter fra vores controller, da vi ønsker at opdatere
+            ArrayList<User> users = UserController.getUsers();
+
+            //Setter her produkterne for instancerne og for created timestamp
+            this.users = users;
+            this.created = System.currentTimeMillis() / 1000L;
+
+        }
+
+        //Returnerer dokumenter
+        return this.users;
+
+        }
+    }
+
+
+
