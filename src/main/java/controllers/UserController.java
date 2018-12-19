ec88fed13801dd143247e5dee5e623e8851bddb2
alihan <alihan1996@live.com>
2018-12-19 22:50:44 +0100

    Hashing the user password before saving it

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -1,9 +1,14 @@
 package controllers;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+
+import com.sun.javafx.scene.traversal.Algorithm;
+import com.sun.org.apache.xml.internal.security.algorithms.JCEMapper;
 import model.User;
+import utils.Hashing;
 import utils.Log;
 
 public class UserController {
@@ -95,6 +100,8 @@ public class UserController {
 
   public static User createUser(User user) {
 
+    Hashing hashwhatever = new Hashing();
+
     // Write in log that we've reach this step
     Log.writeLog(UserController.class.getName(), user, "Actually creating a user in DB", 0);
 
@@ -107,14 +114,17 @@ public class UserController {
     }
 
     // Insert the user in the DB
-    // TODO: Hash the user password before saving it.
+    // TODO: Hash the user password before saving it. - FIXED
+
+
     int userID = dbCon.insert(
         "INSERT INTO user(first_name, last_name, password, email, created_at) VALUES('"
             + user.getFirstname()
             + "', '"
             + user.getLastname()
             + "', '"
-            + user.getPassword()
+                //Hasher her password
+            + hashwhatever.HashSalt(user.getPassword()
             + "', '"
             + user.getEmail()
             + "', "
@@ -132,4 +142,107 @@ public class UserController {
     // Return user
     return user;
   }
+
+
+  public static void deleteUser (int id) {
+
+    //Tjekker efter for at se om der er en forbindelse til databasen
+
+    if (dbCon == null) {
+      dbCon = new DatabaseController();
+    }
+
+    //Her kommer der et prepared statement
+    String sql = "DELETE FROM user WHERE id=" + id;
+
+    dbCon.deleteUser(sql);
+  }
+
+  public static String loginUser(User user) {
+
+    //Tjekker efter for at se om databasen er forbundet
+
+    if (dbCon == null) {
+      dbCon = new DatabaseController();
+    }
+
+    String sql = "SELECT * FROM user where the email=" + user.getEmail() + "AND password" + user.getPassword() + "";
+
+    dbCon.loginUser (sql);
+
+    //Så skaber vi en query
+    ResultSet resultSet = dbCon.query(sql);
+    User userlogin
+    String token = null
+
+    try {
+      //Da vi kun har et objekt, er det det eneste vi getter
+      if (resultSet.next()) {
+        userlogin = new User(
+                resultSet.getInt("ID"),
+                resultSet.getInt("first_name"),
+                resultSet.getInt("last_name"),
+                resultSet.getInt("password"),
+                resultSet.getInt("email"),
+
+                if (userlogin != null) {
+                  try {
+
+                    Algorithm algorithm = Algorithm.HMAC256("Secret");
+                    token = JWT.create()
+                            .withClaim("UserID", userlogin.getId())
+                            .withIssuer("auth0")
+                            .sign(algorithm);
+                  } catch (JWTCreationException exception) {
+                  //Forkert sign konfiguration / kunne altså ikke konvertere claims
+                  System.out.println(exception.getMessage());
+                } finally {
+                  return token;
+                  }
+                }
+      } else {
+      System.out.println("There was no user found");
+    }
+  } catch (SQLException ex) {
+
+    System.out.println(ex.getMessage());
+  }
+    //Returnerer null
+
+  return "";
+}
+
+public static Boolean updateUser(User user, String token) {
+  if (dbCon == null) {
+    dbCon = new DatabaseController();
+  }
+
+  try {
+    DecodedJWT jwt = JWT.decode(token);
+    int id = jwt.getClaim("UserID").asInt();
+
+    try {
+      PreparedStatement updateUser = dbCon.getConnection().prepareStatement("UPDATE USER SET" + "first_name  = ?, last name = ?, password = ?, email = ?, WHERE id = ?");
+      updateUser.setString(1, user.getFirstname());
+      updateUser.setString(2, user.getLastname());
+      updateUser.setString(3, user.getPassword());
+      updateUser.setString(4, user.getEmail());
+      updateUser.setInt(5, id);
+      int rowsaffected = updateUser.executeUpdate();
+
+      if (rowsaffected == 1) {
+        return true;
+
+      }
+
+    } catch (SQLException sql) {
+      sql.printStackTrace();
+
+    }
+
+  } catch (JWTCreationException exception) {
+    exception.printStackTrace();
+  }
+  return false;
+}
 }
